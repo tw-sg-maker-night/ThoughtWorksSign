004dbf2755334efd40a4eef24c1b1b83baa74c79
Yu Lian Ng <ng_yl88@hotmail.com>
2019-04-10 22:58:43 +0800

    add files for ESP32

---
+++ ThoughtWorks_Sign/secret.h
@@ -0,0 +1,6 @@
+const char* ssid     = "";
+const char* password = "";
+
+//const char* mqttClientId  = "mqttClient";
+//const char* mqttUsername = "mqttUser";
+//const char* mqttPassword = "mqttPassword";
